b261b2713bbd5d7d14aceb02f7c04358131a0150
XuanKha <nguyenxuankhka5371@GMAIL.COM>
2023-04-16 18:36:38 +0700

    first push front end

---
+++ src/Register.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react"
+
+const Register = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  return (
+    <div className="bg-blue-50 h-screen flex  items-center">
+      <form className="w-64 mx-auto mb-12">
+        <input
+          type="text"
+          value={username}
+          placeholder="username"
+          className="block w-full p-2 rounded-sm mb-2 border"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="password"
+          className="block w-full p-2 rounded-sm mb-2 border"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="bg-blue-500 text-white block w-full p-2  rounded-sm">
+          Register
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Register
