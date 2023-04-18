a9edbe68d3116fd9bb4b667004f1576eff0b7de7
XuanKha <nguyenxuankhka5371@GMAIL.COM>
2023-04-18 14:29:38 +0700

    fix Api

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import axios from "axios"
 import "./App.css"
 import Routes from "./Routes"
 import { UserContextProvider } from "./UserContext"
+import { disableReactDevTools } from "@fvilers/disable-react-devtools"
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools()
+}
 
 function App() {
-  axios.defaults.baseURL = "https://chat-app-api-chi.vercel.app"
+  axios.defaults.baseURL = "https://chat-app-api-uie4.onrender.com"
   axios.defaults.withCredentials = true
   return (
     <UserContextProvider>
--- src/Chat.jsx
+++ src/Chat.jsx
@@ -19,8 +19,7 @@ export default function Chat() {
     connectToWs()
   }, [selectedUserId])
   function connectToWs() {
-    const ws = new WebSocket("ws://chat-app-api-chi.vercel.app")
-    console.log("🚀 Kha ne ~ file: Chat.jsx:23 ~ ws:", ws)
+    const ws = new WebSocket("ws://localhost:4000")
     setWs(ws)
     ws.addEventListener("message", handleMessage)
     ws.addEventListener("close", () => {
