c01fe6134f6e5e64c14e77dfa98492e8ca624b41
XuanKha <nguyenxuankhka5371@GMAIL.COM>
2023-04-18 15:25:56 +0700

    update base URL

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,9 @@ import axios from "axios"
 import "./App.css"
 import Routes from "./Routes"
 import { UserContextProvider } from "./UserContext"
-import { disableReactDevTools } from "@fvilers/disable-react-devtools"
-
-if (process.env.NODE_ENV === "production") {
-  disableReactDevTools()
-}
 
 function App() {
-  axios.defaults.baseURL = "https://chat-app-api-uie4.onrender.com"
+  axios.defaults.baseURL = "https://chat-app-m9zj.onrender.com"
   axios.defaults.withCredentials = true
   return (
     <UserContextProvider>
