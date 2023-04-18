b61365a03e8a5f702494ad17e41ad831a00174ad
XuanKha <nguyenxuankhka5371@GMAIL.COM>
2023-04-18 16:17:44 +0700

    change websocket

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -19,7 +19,7 @@ export default function Chat() {
     connectToWs()
   }, [selectedUserId])
   function connectToWs() {
-    const ws = new WebSocket("ws://chat-app-api-uie4.onrender.com")
+    const ws = new WebSocket("ws://chat-app-api-uie4.onrender.com:4000")
     setWs(ws)
     ws.addEventListener("message", handleMessage)
     ws.addEventListener("close", () => {
