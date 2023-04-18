af244d0c4abade972ce6a2b3a0f50950264d46cb
XuanKha <nguyenxuankhka5371@GMAIL.COM>
2023-04-18 12:21:18 +0700

    finish UI

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { UserContextProvider } from "./UserContext"
 function App() {
   axios.defaults.baseURL = "http://localhost:4000"
   axios.defaults.withCredentials = true
-
   return (
     <UserContextProvider>
       <Routes />
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react"
+import Chat from "./Chat"
 import Register from "./RegisterAndLoginForm"
 import { UserContext } from "./UserContext"
 
 const Routes = () => {
   const { username } = useContext(UserContext)
   if (username) {
-    return "logged in" + username
+    return <Chat/>
   }
 
   return <Register />
